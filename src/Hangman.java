7ddf327265db03227b7f2c377d1552ae27313ace
guglielmo-andrew <guglielmo.andrew@gmail.com>
2019-10-20 23:16:46 -0700

    Removed a redundant line on the welcome message and added a couple comments.

--- src/Hangman.java
+++ src/Hangman.java
@@ -17,7 +17,7 @@ public class Hangman {
         }
 
         Gameplay game = new Gameplay(word);
-        System.out.println("Welcome to Hangman");
+
         System.out.println("db   d8b   db d88888b db       .o88b.  .d88b.  .88b  d88. d88888b      d888888b  .d88b.       db   db  .d8b.  d8b   db  d888b  .88b  d88.  .d8b.  d8b   db \n" +
                 "88   I8I   88 88'     88      d8P  Y8 .8P  Y8. 88'YbdP`88 88'          `~~88~~' .8P  Y8.      88   88 d8' `8b 888o  88 88' Y8b 88'YbdP`88 d8' `8b 888o  88 \n" +
                 "88   I8I   88 88ooooo 88      8P      88    88 88  88  88 88ooooo         88    88    88      88ooo88 88ooo88 88V8o 88 88      88  88  88 88ooo88 88V8o 88 \n" +
